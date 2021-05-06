38c31bc4698fa2d5a71818d112a72ac6d2e0d864
Hedayat Abedijoo <h.abedijoo@gmail.com>
2021-05-06 19:26:33 +0200

    Update common.ts

--- holo-layer/tests/src/common.ts
+++ holo-layer/tests/src/common.ts
@@ -4,105 +4,67 @@ import path from "path";
 
 export const conductorConfig = Config.gen({});
 export const Peershare_Zome = "peershare";
-
+export const Default_Developer_Address =
+  "uhCAkbs0mibHm7rB0ZPkZZzhHC55KwmDxt6uR9x2XgFQ82OYvKwR7";
 export const peerShareDNA = path.join(
-    __dirname,
-    "../../workdir/dna/peershare.dna"
+  __dirname,
+  "../../workdir/dna/peershare.dna"
 );
 
 const installationAgents: InstallAgentsHapps = [
-    // agent 0
-    [
-        // happ 0
-        [peerShareDNA],
-    ],
-    [
-        // happ 0
-        [peerShareDNA],
-    ],
+  // agent 0
+  [
+    // happ 0
+    [peerShareDNA],
+  ],
+  [
+    // happ 0
+    [peerShareDNA],
+  ],
 ];
 
-export const InstallAgentApp = async (s, agentName: string, agentKey: string = "") => {
-    const config = Config.gen();
-    const [agent] = await s.players([conductorConfig], false);
-    await agent.startup();
-    const adminWs = agent.adminWs();
-    let agent_key = Buffer.from(agentKey);
-    if(agentKey == ""){
-        agent_key = await adminWs.generateAgentPubKey();
-    }
-    const hash_app = await adminWs.registerDna({
-        path: peerShareDNA,
-    });
+import { Base64 } from "js-base64";
+export function serializeHash(hash) {
+  return `u${Base64.fromUint8Array(hash, true)}`;
+}
+export const InstallAgentApp = async (
+  s,
+  agentName: string,
+  am_i_developer: boolean = false
+) => {
+  const config = Config.gen();
+  const [agent] = await s.players([conductorConfig], false);
+  await agent.startup();
+  const adminWs = agent.adminWs();
+  let progenitor: string = Default_Developer_Address;
+  var agent_key = await adminWs.generateAgentPubKey();
+  if (am_i_developer) {
+    progenitor = serializeHash(agent_key);
+  }
+  const hash_app = await adminWs.registerDna({
+    path: peerShareDNA,
+    properties: {
+      developer_address: progenitor,
+    },
+  });
 
-    const req: InstallAppRequest = {
-        installed_app_id: `peer-share-app-` + agentName,
-        agent_key: agent_key,
-        dnas: [
-            {
-                hash: hash_app,
-                nick: "my_cell_" + agentName,
-            },
-        ],
-    };
-    const installedHapp = await agent._installHapp(req);
+  const req: InstallAppRequest = {
+    installed_app_id: `peer-share-app-` + agentName,
+    agent_key: agent_key,
+    dnas: [
+      {
+        hash: hash_app,
+        nick: "my_cell_" + agentName,
+      },
+    ],
+  };
+  const installedHapp = await agent._installHapp(req);
 
-    return installedHapp.cells[0];
+  return installedHapp.cells[0];
 };
 
-
-export const InstallAgentApp_As_Developer = async (s, agentName: string) => {
-    const config = Config.gen();
-    const [agent] = await s.players([conductorConfig], false);
-    await agent.startup();
-    const adminWs = agent.adminWs();
-    const agent_key = await _generateAgentPubKey_without_special_charachter(
-        adminWs
-    );
-    //const agent_key = await adminWs.generateAgentPubKey();
-
-    var developer_address = agent_key.toString("base64");
-    _log("Generated_Agent_Key_For_Test", developer_address);
-
-    const hash_app = await adminWs.registerDna({
-        path: peerShareDNA,
-        properties: { developer_address: developer_address },
-    });
-
-    const req: InstallAppRequest = {
-        installed_app_id: `peer-share-app-` + agentName,
-        agent_key: agent_key,
-        dnas: [
-            {
-                hash: hash_app,
-                nick: "my_cell_" + agentName,
-            },
-        ],
-    };
-    const installedHapp = await agent._installHapp(req);
-
-    return installedHapp.cells[0];
-};
 export const _log = (key, value) => {
-    console.log("######(" + key + ")#############");
-    console.log(value);
-    console.log("-----------------------------------");
+  console.log("######(" + key + ")#############");
+  console.log(value);
+  console.log("-----------------------------------");
 };
-
-
-/**************************Generate safe key  */
-async function _generateAgentPubKey_without_special_charachter(adminWs) {
-    var generated_key = await adminWs.generateAgentPubKey();
-    while (_key_contains_special_chars(generated_key.toString("base64"))) {
-        generated_key = await adminWs.generateAgentPubKey();
-    }
-    return generated_key;
-}
-var format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
-function _key_contains_special_chars(val) {
-    if (format.test(val)) {
-        return true;
-    } else {
-        return false;
-    }
-}
