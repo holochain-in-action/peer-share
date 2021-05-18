64d15de451ade0dbcf728452b39f68e536e6f6ba
hedayat <h.abedijoo@gmail.com>
2021-05-18 16:39:37 +0200

    refactor common.ts for suporting multi agent tests

--- holo-layer/tests/src/common.ts
+++ holo-layer/tests/src/common.ts
@@ -11,20 +11,6 @@ export const peerShareDNA = path.join(
   "../../workdir/dna/peershare.dna"
 );
 
-// const installationAgent: InstallAgentsHapps = [[[peerShareDNA]]];
-
-// const installationAgents: InstallAgentsHapps = [
-//   // agent 0
-//   [
-//     // happ 0
-//     [peerShareDNA],
-//   ],
-//   [
-//     // happ 0
-//     [peerShareDNA],
-//   ],
-// ];
-
 import { Base64 } from "js-base64";
 export function serializeHash(hash) {
   return `u${Base64.fromUint8Array(hash, true)}`;
@@ -67,29 +53,16 @@ export const InstallAgentApp = async (
 
 export const InstallAgentsApp = async (s, agentNames: string[]) => {
   const config = Config.gen();
-  const [agent_dev, agent_nondev] = await s.players(
-    [conductorConfig, conductorConfig],
-    false
-  );
-  await agent_dev.startup();
-  await agent_nondev.startup();
-  //return [0, 1];
-  const adminWs_dev = agent_dev.adminWs();
-  const adminWs_nondev = agent_nondev.adminWs();
+  const [agent] = await s.players([conductorConfig], false);
+  await agent.startup();
+  const adminWs = agent.adminWs();
   let progenitor: string = Default_Developer_Address;
-  var agent_developer_key = await adminWs_dev.generateAgentPubKey();
-  var agent_non_developer_key = await adminWs_nondev.generateAgentPubKey();
+  var agent_developer_key = await adminWs.generateAgentPubKey();
+  var agent_non_developer_key = await adminWs.generateAgentPubKey();
 
   progenitor = serializeHash(agent_developer_key);
 
-  const hash_app_dev = await adminWs_dev.registerDna({
-    path: peerShareDNA,
-    properties: {
-      developer_address: progenitor,
-    },
-  });
-
-  const hash_app_nondev = await adminWs_nondev.registerDna({
+  const hash_dna = await adminWs.registerDna({
     path: peerShareDNA,
     properties: {
       developer_address: progenitor,
@@ -101,7 +74,7 @@ export const InstallAgentsApp = async (s, agentNames: string[]) => {
     agent_key: agent_developer_key,
     dnas: [
       {
-        hash: hash_app_dev,
+        hash: hash_dna,
         nick: "my_cell_" + agentNames[0],
       },
     ],
@@ -112,16 +85,14 @@ export const InstallAgentsApp = async (s, agentNames: string[]) => {
     agent_key: agent_non_developer_key,
     dnas: [
       {
-        hash: hash_app_nondev,
+        hash: hash_dna,
         nick: "my_cell_" + agentNames[1],
       },
     ],
   };
 
-  const developer_agent = await agent_dev._installHapp(req_for_developer);
-  const non_developer_agent = await agent_nondev._installHapp(
-    req_for_non_developer
-  );
+  const developer_agent = await agent._installHapp(req_for_developer);
+  const non_developer_agent = await agent._installHapp(req_for_non_developer);
   return [developer_agent.cells[0], non_developer_agent.cells[0]];
 };
 
