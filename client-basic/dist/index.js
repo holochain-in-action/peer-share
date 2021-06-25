e2b4fd6a499a7a866e5e2002867d48d0b9bcbf6b
hedayat <h.abedijoo@gmail.com>
2021-06-25 15:18:42 +0200

    debuggin sending payload to zome

--- client-basic/dist/index.js
+++ client-basic/dist/index.js
@@ -5021,48 +5021,51 @@ __webpack_require__.r(__webpack_exports__);
 
 window.Buffer = __webpack_require__(/*! buffer/ */ "./node_modules/buffer/index.js").Buffer; // hack
 
-
 class Index {
-    constructor() {
-        let btn = document.getElementById("btnCallZomeFn");
-        btn?.addEventListener("click", (e) => this.click(e));
-    }
+  constructor() {
+    let btn = document.getElementById("btnCallZomeFn");
+    btn?.addEventListener("click", (e) => this.click(e));
+  }
 
-    async click(e) {
-        const appid = document.getElementById("txt_appid").value;
-        const port = document.getElementById("txt_port").value;
-        const fnname = document.getElementById("txt_fn").value;
-        const payload = document.getElementById("txt_payload").value;
+  async click(e) {
+    const appid = document.getElementById("txt_appid").value;
+    const port = document.getElementById("txt_port").value;
+    const fnname = document.getElementById("txt_fn").value;
+    const payload = document.getElementById("txt_payload").value;
 
-        const result = await this.call_zome_fn(port, appid, fnname, payload);
+    const result = await this.call_zome_fn(port, appid, fnname, payload);
 
-        console.log(result);
-    }
+    console.log(result);
+  }
 
-    async call_zome_fn(port, appid, fnname, payload = null) {
-        const appConnection = await _holochain_conductor_api__WEBPACK_IMPORTED_MODULE_0__.AppWebsocket.connect("ws://localhost:" + port);
-        const appInfo = await appConnection.appInfo({
-            installed_app_id: appid,
-        });
-        const cellId = appInfo.cell_data[0].cell_id;
-        // try {
-        const param = {
-            cap: null,
-            cell_id: cellId,
-            zome_name: "peershare",
-            fn_name: fnname,
-            provenance: cellId[1],
-            payload: payload,
-        };
-        const message = await appConnection.callZome(param);
-        return message;
-        // } catch (error) {
-        //   return error;
-        // }
-    }
+  async call_zome_fn(port, appid, fnname, payload = null) {
+    const appConnection = await _holochain_conductor_api__WEBPACK_IMPORTED_MODULE_0__.AppWebsocket.connect("ws://localhost:" + port);
+    const appInfo = await appConnection.appInfo({
+      installed_app_id: appid,
+    });
+    const cellId = appInfo.cell_data[0].cell_id;
+    // try {
+    const param = {
+      cap: null,
+      cell_id: cellId,
+      zome_name: "peershare",
+      fn_name: fnname,
+      provenance: cellId[1],
+    };
+
+    // add Payload to Param if there is a value to send to zome
+    if (payload && !/\s/.test(payload)) param.payload = payload;
+
+    const message = await appConnection.callZome(param);
+    return message;
+    // } catch (error) {
+    //   return error;
+    // }
+  }
 }
 
 new Index();
+
 })();
 
 /******/ })()
--- client-basic/src/index.js
+++ client-basic/src/index.js
@@ -2,45 +2,47 @@ import { AppWebsocket } from "@holochain/conductor-api";
 
 window.Buffer = require("buffer/").Buffer; // hack
 
-
 class Index {
-    constructor() {
-        let btn = document.getElementById("btnCallZomeFn");
-        btn?.addEventListener("click", (e) => this.click(e));
-    }
+  constructor() {
+    let btn = document.getElementById("btnCallZomeFn");
+    btn?.addEventListener("click", (e) => this.click(e));
+  }
 
-    async click(e) {
-        const appid = document.getElementById("txt_appid").value;
-        const port = document.getElementById("txt_port").value;
-        const fnname = document.getElementById("txt_fn").value;
-        const payload = document.getElementById("txt_payload").value;
+  async click(e) {
+    const appid = document.getElementById("txt_appid").value;
+    const port = document.getElementById("txt_port").value;
+    const fnname = document.getElementById("txt_fn").value;
+    const payload = document.getElementById("txt_payload").value;
 
-        const result = await this.call_zome_fn(port, appid, fnname, payload);
+    const result = await this.call_zome_fn(port, appid, fnname, payload);
 
-        console.log(result);
-    }
+    console.log(result);
+  }
 
-    async call_zome_fn(port, appid, fnname, payload = null) {
-        const appConnection = await AppWebsocket.connect("ws://localhost:" + port);
-        const appInfo = await appConnection.appInfo({
-            installed_app_id: appid,
-        });
-        const cellId = appInfo.cell_data[0].cell_id;
-        // try {
-        const param = {
-            cap: null,
-            cell_id: cellId,
-            zome_name: "peershare",
-            fn_name: fnname,
-            provenance: cellId[1],
-            payload: payload,
-        };
-        const message = await appConnection.callZome(param);
-        return message;
-        // } catch (error) {
-        //   return error;
-        // }
-    }
+  async call_zome_fn(port, appid, fnname, payload = null) {
+    const appConnection = await AppWebsocket.connect("ws://localhost:" + port);
+    const appInfo = await appConnection.appInfo({
+      installed_app_id: appid,
+    });
+    const cellId = appInfo.cell_data[0].cell_id;
+    // try {
+    const param = {
+      cap: null,
+      cell_id: cellId,
+      zome_name: "peershare",
+      fn_name: fnname,
+      provenance: cellId[1],
+    };
+
+    // add Payload to Param if there is a value to send to zome
+    if (payload && !/\s/.test(payload)) param.payload = payload;
+
+    const message = await appConnection.callZome(param);
+    return message;
+    // } catch (error) {
+    //   return error;
+    // }
+  }
 }
 
 new Index();
